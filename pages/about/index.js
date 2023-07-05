a385b2716f3a27c7c00e3abf82880bc1facabc1a
ngoisao962 <luongvanchuong963@gmail.com>
2023-07-05 16:26:17 +0700

    first commit

---
+++ pages/about/index.js
@@ -0,0 +1,180 @@
+import React, { useState } from 'react';
+
+// icons
+import {
+  FaHtml5,
+  FaBootstrap,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaVuejs,
+  FaWordpress,
+  FaFigma,
+} from 'react-icons/fa';
+
+import {
+  SiNextdotjs,
+  SiFramer,
+  SiAdobexd,
+  SiAdobephotoshop,
+} from 'react-icons/si';
+
+//  about data
+export const aboutData = [
+  {
+    title: 'skills',
+    info: [
+      {
+        title: 'Web Development',
+        icons: [
+          <FaHtml5 />,
+          <FaCss3 />,
+          <FaBootstrap/>,
+          <FaJs />,
+          <FaReact />,
+          <FaVuejs/>,
+          
+        ],
+      },
+      {
+        title: 'UI/UX Design',
+        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+      },
+    ],
+  },
+  
+  {
+    title: 'experience',
+    info: [
+      {
+        title: 'The Web Developer Bootcamp (HTML, CSS, JS, Scss, Boostrap, Jquery,Tailwind...) on Udemy',
+        stage: '2020-2021',
+      },
+      {
+        title: ' React -The Complete Guide (incl Hooks, React Router, Redux) on Udemy',
+        stage: '2021-2022',
+      },
+      {
+        title: 'Intern - Frontend Developer',
+        stage: '03/2023 - 06/2023',
+      },
+    ],
+  },
+ 
+];
+
+// components
+import Avatar from '../../components/Avatar';
+import Circles from '../../components/Circles';
+
+// framer motion
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+
+// counter
+import CountUp from 'react-countup';
+
+const About = () => {
+  const [index, setIndex] = useState(0);
+  console.log(index);
+  return (
+    <div className='h-full bg-primary/30 py-32 text-center xl:text-left'>
+      <Circles />
+      {/* avatar img */}
+      <motion.div
+        variants={fadeIn('right', 0.2)}
+        initial='hidden'
+        animate='show'
+        exit='hidden'
+        className='hidden xl:flex absolute bottom-0 -left-[370px]'
+      >
+        <Avatar />
+      </motion.div>
+      <div className='container mx-auto h-full flex justify-center flex-col items-center xl:flex-row gap-x-6'>
+        {/* text */}
+        <div className='flex-1 flex flex-col justify-center'>
+          <motion.h2
+            variants={fadeIn('right', 0.2)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='h2'
+          >
+             <span className='text-accent'>CAREER </span> GOALS
+          </motion.h2>
+          <motion.p
+            variants={fadeIn('right', 0.4)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'
+          >
+            Become a senior professional full-stack
+developer, especially with React ecosystem, including
+mobile and web applications, in 5 years
+ 
+
+          </motion.p>
+          {/* counters */}
+          <motion.div
+            variants={fadeIn('right', 0.6)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8'
+          >
+           
+          </motion.div>
+        </div>
+        {/* info */}
+        <motion.div
+          variants={fadeIn('left', 0.4)}
+          initial='hidden'
+          animate='show'
+          exit='hidden'
+          className='flex flex-col w-full xl:max-w-[48%] justify-center h-[480px]'
+        >
+          <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
+            {aboutData.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className={`${
+                    index === itemIndex &&
+                    'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'
+                  }  cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                  onClick={() => setIndex(itemIndex)}
+                >
+                  {item.title}
+                </div>
+              );
+            })}
+          </div>
+          <div className='py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start'>
+            {aboutData[index].info.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'
+                >
+                  {/* title */}
+                  <div className='font-light mb-2 md:mb-0'>{item.title}</div>
+                  <div className='hidden md:flex'>:</div>
+                  <div>{item.stage}</div>
+                  <div className='flex gap-x-4'>
+                    {/* icons */}
+                    {item.icons?.map((icon, itemIndex) => {
+                      return <div className='text-2xl text-white'>{icon}</div>;
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
